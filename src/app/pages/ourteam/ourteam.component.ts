e5e4aa375ffb6f9d5be34c400d324a65f59fcaf7
Leo <rick.shrotri@gmail.com>
2019-08-24 10:01:59 -0700

    commit9

--- src/app/pages/ourteam/ourteam.component.ts
+++ src/app/pages/ourteam/ourteam.component.ts
@@ -1,9 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-ourteam',
   templateUrl: './ourteam.component.html',
-  styleUrls: ['./ourteam.component.css']
+
+  styles: [`
+    .dark-modal .modal-content {
+      background-color: #000000;
+      color: white;
+      border-radius : 10px;
+    }
+    .dark-modal .modal-header {
+      border : none
+    }
+    .dark-modal .close {
+      color: white;
+    }
+  `]
 })
 export class OurteamComponent implements OnInit {
 
